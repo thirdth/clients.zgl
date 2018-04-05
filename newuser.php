0d5864ac0d992437b314f39d429dc7313a17d0ad
Zack Glaser <zackglaser@gmail.com>
2018-04-04 22:30:39 -0500

    signup border start

--- newuser.php
+++ newuser.php
@@ -3,7 +3,7 @@ include 'includes/header.php'; ?>
 <div class="container">
   <div class="col-md-2">
   </div>
-  <div class="col-md-8">
+  <div class="col-md-8 signup">
     <h3>Login</h3>
   </div>
 </div>
