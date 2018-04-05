7cbe168450634116d9ff8a5893278a40a2fa9892
Zack Glaser <zackglaser@gmail.com>
2018-04-04 22:26:02 -0500

    added container

--- newuser.php
+++ newuser.php
@@ -1,5 +1,12 @@
 <?php
 include 'includes/header.php'; ?>
+<div class="container-fluid">
+  <div class="col-md-2">
+  </div>
+  <div class="col-md-8">
+    <h3>Login</h3>
+  </div>
+</div>
 
 <?php
 include 'includes/footer.php'; ?>
