6472acc489d9e38f4e0c3533d56dbfbe791922a3
Zack Glaser <zackglaser@gmail.com>
2018-04-25 13:53:54 -0500

    changed addy of garble.cnfg

--- inserts/insertUser.php
+++ inserts/insertUser.php
@@ -1,7 +1,7 @@
 <?php
 error_reporting(-1);
 ini_set('display_errors', 'On');
-include 'includes/garble_cnfg.php';
+include '../includes/garble_cnfg.php';
 if(isset($_POST['name']) && isset($_POST['password'])) {
   register_user($_POST['name'], $_POST['password'], $_POST['email']);
   header("Location: index.php");
