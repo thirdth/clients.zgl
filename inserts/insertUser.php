8984d653f397ae341d8d2d46d70a368e1830e0cb
Zack Glaser <zackglaser@gmail.com>
2018-04-25 13:54:57 -0500

    changed addy of index.php

--- inserts/insertUser.php
+++ inserts/insertUser.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 'On');
 include '../includes/garble_cnfg.php';
 if(isset($_POST['name']) && isset($_POST['password'])) {
   register_user($_POST['name'], $_POST['password'], $_POST['email']);
-  header("Location: index.php");
+  header("Location: ../index.php");
   die();
 }else {
   header("Location: ../register.php?error=error");
