a4e713f20ce04965a9a244ace95021cda4579ccd
Zack Glaser <zackglaser@gmail.com>
2018-04-25 14:16:39 -0500

    changed includes/garble

--- logout.php
+++ logout.php
@@ -1,7 +1,7 @@
 <?php
 error_reporting(-1);
 ini_set('display_errors', 'On');
-include 'garble_cnfg.php';
+include 'includes/garble_cnfg.php';
 clear_session_cookies();
 header("location: login.php");
 ?>
