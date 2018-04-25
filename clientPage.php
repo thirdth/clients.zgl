25245db69ab97f3c60458e646d2634067b96213a
Zack Glaser <zackglaser@gmail.com>
2018-04-25 17:41:45 -0500

    added clientPage

---
+++ clientPage.php
@@ -0,0 +1,16 @@
+<?php
+include 'includes/garble_cnfg.php';
+protected_page();
+header_check();
+?>
+<div class="wrapper container">
+  <div class="col-md-4">
+    <h3>Clients</h3>
+
+  <a href="addClient.php">Add Client</a>
+  </div>
+</div>
+
+
+<?php
+include 'includes/footer.php'; ?>
