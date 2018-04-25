c0307083b9e04cdd38a306f100e223a5576c18ee
Zack Glaser <zackglaser@gmail.com>
2018-04-25 13:52:06 -0500

    display errors in insertUser

--- inserts/insertUser.php
+++ inserts/insertUser.php
@@ -1,4 +1,6 @@
 <?php
+error_reporting(-1);
+ini_set('display_errors', 'On');
 include 'includes/garble_cnfg.php';
 if(isset($_POST['name']) && isset($_POST['password'])) {
   register_user($_POST['name'], $_POST['password'], $_POST['email']);
