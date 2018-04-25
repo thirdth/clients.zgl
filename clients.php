8db857684d56995c6f200137c1db40d8c89c8123
Zack Glaser <zackglaser@gmail.com>
2018-04-25 17:21:22 -0500

    added last name

--- clients.php
+++ clients.php
@@ -3,18 +3,15 @@ include 'includes/garble_cnfg.php';
 protected_page();
 header_check();
 $clients = get_clients();
-print_r($clients);
 ?>
 <div class="wrapper container">
   <?php
 foreach ($clients as $client){
-  echo $client['FName'] . "</br>";
+  echo $client['FName'] . " " . $client['LName'] . "</br>";
 }
    ?>
   <a href="addClient.php">Add Client</a>
 </div>
-<?php echo $clients[0]['FName']; ?>
-
 
 
 <?php
