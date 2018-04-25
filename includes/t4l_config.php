afa4bea7e2586f7021f5e83e451a032f15e17572
Zack Glaser <zackglaser@gmail.com>
2018-04-25 12:26:22 -0500

    took connect out of t4lconfig

--- includes/t4l_config.php
+++ includes/t4l_config.php
@@ -1,19 +1,5 @@
 <?php
 
-// database functions
-// TODO: confirm that this DB is protected
-function get_connected()  {
-  static $conn;
-  if (!isset($conn))  {
-    $db = parse_ini_file($_SERVER['DOCUMENT_ROOT'] . '/config/config.ini');
-    $conn = mysqli_connect($db['host'], $db['user'], $db['pass'], $db['name']);
-  }
-  if (!$conn) {
-    die("Connection Failed: " . mysqli_connect_error());
-  }
-  return $conn;
-}
-
 function shorten_string($string, $wordsreturned)
 /*  Returns the first $wordsreturned out of $string.  If string
 contains fewer words than $wordsreturned, the entire string
