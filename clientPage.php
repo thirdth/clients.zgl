52b0f124c14cddb0eeb75e41423e00392a2f21cc
Zack Glaser <zackglaser@gmail.com>
2018-04-25 19:42:06 -0500

    editing clientPage

--- clientPage.php
+++ clientPage.php
@@ -5,10 +5,11 @@ header_check();
 $clientID = $_GET['ID'];
 $client = get_client_byId($clientID);
 print_r($client);
+// TODO: add matters here
 ?>
 <div class="wrapper container">
   <div class="col-md-4">
-    <h3>Clients</h3>
+    <h3>Client Information</h3>
 
   <a href='editClient.php?ID=" . $clientID . "'>Edit Client</a>
   </div>
