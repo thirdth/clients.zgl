898ee6f301adc51bba05e1ad6e5f7704cfc239e7
Zack Glaser <zackglaser@gmail.com>
2018-04-27 18:03:50 -0500

    formatting date better

--- matter.php
+++ matter.php
@@ -57,7 +57,7 @@ $claims = get_claims_byID($matterID);
             $xactions = get_xaction_byClaimID($claim['ID']);
             foreach ($xactions as $xaction) {
               $phpdate = strtotime($xaction['CreatedDate']);
-              $date = date('Y-m-d', $phpdate);
+              $date = date('M-DD-Y', $phpdate);
               echo "  <div class='form-group col-md-2'>
                         <input type='text' class='form-control' value='" . $date . "' readonly>
                       </div>
