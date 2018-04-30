cf21d0ffd10c73f3edd7d6b48a324d39364928de
Zack Glaser <zackglaser@gmail.com>
2018-04-30 14:00:33 -0500

    updateClaim

--- edits/updateClaim.php
+++ edits/updateClaim.php
@@ -2,6 +2,16 @@
 error_reporting(-1);
 ini_set('display_errors', 'On');
 include '../includes/garble_cnfg.php';
-print_r($_POST);
+$claimID = $_POST['ID'];
+$description = $_POST['Description'];
+$typeID = $_POST['Type'];
+$matterID = $_POST['MatterID']
 
+$result = update_claim($claimID, $description, $typeID);
+
+if ($result)  {
+  header("Location: ../matter.php?ID=" .  $matterID);
+} else {
+  echo "something went wrong" . mysqli_error($conn);
+}
 ?>
