2934146d8ecf0569e315a3855c4e1ebd4e8a39a4
Zack Glaser <zackglaser@gmail.com>
2018-04-30 14:55:28 -0500

    will edit matter display?

---
+++ editMatter.php
@@ -0,0 +1,58 @@
+<?php
+error_reporting(-1);
+ini_set('display_errors', 'On');
+include 'includes/garble_cnfg.php';
+protected_page();
+header_check();
+$matterID = $_GET['ID'];
+$matter = get_matter_byID($matterID);
+
+$client = get_client_byID($matter['ClientID']);
+$adverse = get_person_byID($matter['AdverseID']);
+
+?>
+<div class="wrapper container-fluid">
+  <div class="col-md-2">
+    sidebar
+  </div>
+  <div class="col-md-10">
+      <h2>Edit: <?php echo $matter['Name']; ?></h2>
+      <div class='col-md-12'>
+        <h3>Client Information</h3>
+        <a href="editClient.php?ID=<?php echo $matter['ClientID']; ?>" class="btn btn-success btn-sm">Edit Client</a>
+      </div>
+      <hr>
+      <form action="edits/updateMatter.php" method="post">
+          <div class="form-group">
+              <input type="hidden" name="ID" value="<?php echo $matterID; ?>">
+              <label>Name:</label>
+              <input type="text" name="Name" class="form-control" value="<?php echo $Name; ?>">
+              <span class="help-block"></span>
+          </div>
+          <div class="form-group">
+              <label>Notes</label>
+              <input type="text" name="Notes" class="form-control" value="<?php echo $Notes; ?>">
+              <span class="help-block"></span>
+          </div>
+          <div class="form-group">
+              <label>First Name</label>
+              <input type="text" name="FName" class="form-control" value="<?php echo $Adverse['Fname']; ?>">
+              <span class="help-block"></span>
+          </div>
+          <div class="form-group">
+              <label>Last Name</label>
+              <input type="text" name="LName" class="form-control" value="<?php echo $Adverse['LName']; ?>">
+              <span class="help-block"></span>
+          </div>
+          <div class="form-group">
+              <button type="submit" class="btn btn-success" name="submit" value="Submit">Submit</button>
+              <a href="deletes/deleteMatter.php?ID=<?php echo $matterID; ?>" class="btn btn-danger btn-sm pull-right">delete Matter</a>
+          </div>
+      </form>
+    </div>
+  </div>
+
+
+
+<?php
+include 'includes/footer.php'; ?>
