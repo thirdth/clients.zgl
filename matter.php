9e36778cd9650f7fd292a0b4db418204d831f3ae
Zack Glaser <zackglaser@gmail.com>
2018-04-30 13:25:16 -0500

    MatterID

--- matter.php
+++ matter.php
@@ -41,7 +41,7 @@ $claims = get_claims_byID($matterID);
         }
         ?></p>
         <form class="form-group" action="inserts/insertNote.php" method="POST">
-          <input type="hidden" name="matterID" value="<?php echo $matterID; ?>">
+          <input type="hidden" name="MatterID" value="<?php echo $matterID; ?>">
           <div class="form-group">
             <textarea class="form-control" rows="3" name="body"></textarea>
           </div>
