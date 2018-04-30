dd0678a76c8dbb6596cf7c74bd2b403f5512a481
Zack Glaser <zackglaser@gmail.com>
2018-04-30 13:42:05 -0500

    add claimID to post

--- editClaim.php
+++ editClaim.php
@@ -19,6 +19,7 @@ $Type = $claim['TypeID'];
       <p>Please enter Claim information.</p>
       <form action="edits/updateClaim.php" method="post">
           <div class="form-group">
+              <input type="hidden" name="ID" value="<?php echo $claimID; ?>">
               <input type="hidden" name="matterID" value="<?php echo $matterID; ?>">
               <label>Description</label>
               <input type="text" name="Description" class="form-control" value="<?php echo $Description; ?>">
