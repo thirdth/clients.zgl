3fb3f569029b8c7cf58867525a12787cbcc9c544
Zack Glaser <zackglaser@gmail.com>
2018-04-30 14:08:56 -0500

    deleted

--- edits/updateClaim.php
+++ edits/updateClaim.php
@@ -3,10 +3,5 @@ error_reporting(-1);
 ini_set('display_errors', 'On');
 include '../includes/garble_cnfg.php';
 print_r($_POST);
-$claimID = $_POST['ID'];
-$description = $_POST['Description'];
-$typeID = $_POST['Type'];
-$matterID = $_POST['MatterID']
 
-echo $claimID;
 ?>
