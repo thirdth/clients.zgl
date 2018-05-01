b2090abd2dd96891b3f0ea14dd887b06828dacbb
Zack Glaser <zackglaser@gmail.com>
2018-05-01 14:43:04 -0500

    changin to int

--- upload.php
+++ upload.php
@@ -27,7 +27,7 @@ if ($uploadOk == 0) {
 // if everything is ok, try to upload file
 } else {
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        $document = insert_file_byName($fileName, $matterID, $fileType, '1', $target_file)
+        $document = insert_file_byName($fileName, $matterID, $fileType, 1, $target_file)
         if ($document)  {
           echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
           print_r($document);
