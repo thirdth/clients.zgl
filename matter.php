15dc66db41a02fee20779e87bf51ab1e768ecc5a
Zack Glaser <zackglaser@gmail.com>
2018-05-01 15:32:46 -0500

    format

--- matter.php
+++ matter.php
@@ -41,11 +41,8 @@ $documents = get_documents_byMatter($matterID);
   </div>
   <div class='col-md-10'>
     <div class="col-md-12">
-      <div class='col-md-8'>
+      <div class='col-md-12'>
         <h3 class="text-center"><strong><?php echo $matter['Name']; ?></strong>  (<?php echo $matter['CourtNO']; ?>)</h3>
-      </div>
-      <div class='col-md-4'>
-
       </div>
       <hr>
         <div class="col-md-3">
