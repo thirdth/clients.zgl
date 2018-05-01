53d32a1b8d7bf9ac8320fad4828ec5a99a4ed8a1
Zack Glaser <zackglaser@gmail.com>
2018-05-01 16:21:31 -0500

    adding addDate.php

---
+++ addDate.php
@@ -0,0 +1,18 @@
+<?php
+include 'includes/garble_cnfg.php';
+protected_page();
+header_check();
+?>
+<div class="wrapper container">
+  <div class="col-md-3">
+  </div>
+  <div class="col-md-6">
+    <form action="inserts/insertDate.php" method="POST" class="form-group">
+      <input type="hidden" name="MatterID" value="<? echo $matterID; ?>">
+      <input type="date" name="Date">
+      <input type="text" name="Description" placeholder="Description">
+      <input type="text" name="Note" placeholder="Note">
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+  </div>
+</div>
