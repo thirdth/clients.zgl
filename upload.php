8ae2d2841945eeea65a17da88a9b014480a728e5
Zack Glaser <zackglaser@gmail.com>
2018-05-01 12:18:22 -0500

    debugging targetfile

--- upload.php
+++ upload.php
@@ -4,6 +4,8 @@ $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
+print_r($target_file);
+
 // Check if file already exists
 if (file_exists($target_file)) {
     echo "Sorry, file already exists.";
