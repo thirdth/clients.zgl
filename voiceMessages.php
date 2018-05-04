b49fb984cd4d3c817f4d27601faa923e1b819e30
Zack Glaser <zackglaser@gmail.com>
2018-05-04 11:03:55 -0500

    messing with print call

--- voiceMessages.php
+++ voiceMessages.php
@@ -24,8 +24,8 @@ foreach ($client->calls->read() as $call) {
   //print_r($call->direction);
   //echo "  |  \n";
   //echo "\n";
-  //print_r($call);
-  //echo "  |  \n";
+  print_r($call);
+  echo "  |  \n";
   echo "\n<div>
           <p>" . $call->direction . " | " . $call->startTime->date . " | " . $call->from . "</p>
           <a href='recordings.php?callSid=" . $call->sid . "'>Call SID: " . $call->sid . "</a>
