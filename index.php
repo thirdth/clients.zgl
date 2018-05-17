549ebccd48dae18b6e1b4474389f9ff5d8e31ff1
Zack Glaser <zackglaser@gmail.com>
2018-05-17 10:00:02 -0500

    centered welcome

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ header_check();
 <div class="container">
   <div class="row">
     <div class="col-md-12">
-      <h2>Welcome<h2>
+      <h2 class="text-center">Welcome<h2>
     </div>
   </div>
   <div class="row">
