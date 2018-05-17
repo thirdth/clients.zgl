cd8eeea9453ccc903c2912b40809ec96af9615ad
Zack Glaser <zackglaser@gmail.com>
2018-05-17 11:32:11 -0500

    hr

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ header_check();
   </div>
   <div class="row">
     <div class="col-md-8">
-      <h3 class="text-center">Create Document</h3>
+      <h3 class="text-center">Create Document</h3><hr>
       <div class="col-md-4">
         <div class="col-md-12 docButton">
           <a href="/resLease"><h1>&#127968;&#xFE0E;</h1>
