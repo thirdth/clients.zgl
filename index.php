aa092ced4cafc01d3668d87a755aac48e3022587
Zack Glaser <zackglaser@gmail.com>
2018-05-17 14:02:20 -0500

    strong numbers:

--- index.php
+++ index.php
@@ -20,15 +20,15 @@ header_check();
   <div class="row">
     <div class="col-md-8">
       <p class="text-center">Create professional looking leagal documents in 5 easy steps.</p>
-      <h3>1. Create an account, Login, or Continue as a Guest</h3>
+      <h3><strong>1. Create an account, Login, or Continue as a Guest</strong></h3>
       <p>When you create an account, you will have the ability to save your documents and access them at a later date. You can also save time through auto-fill options.</p>
-      <h3>2. Browse and Select a Document</h3>
+      <h3><strong>2. Browse and Select a Document</strong></h3>
       <p>Search through the documents that we offer. When you find one that will fit your needs, select it and begin tayloring it to your needs. If you need help at any time, you can always set-up a consultation with one of our lawyers.</p>
-      <h3>3. Answer the Questions</h3>
+      <h3><strong>3. Answer the Questions</strong></h3>
       <p>Once you have selected your document, you will be able to fill out the form by answering a series of questions that are asked. Once you are done, you will be asked to verify the answers to your questions before moving on.</p>
-      <h3>4. Select Add-ons</h3>
+      <h3><strong>4. Select Add-ons</strong></h3>
       <p>Once you have completed your document, you will be presented with some options that others found helpful when creating the same document. At that time, you can elect to have a licensed Tennessee attorney review your document and your specific situation.</p>
-      <h3>5. Pay and Download</h3>
+      <h3><strong>5. Pay and Download</strong></h3>
       <p>Once you have selected any extra services or related documents you may pay for and download your purchases.</p>
     </div>
     <div class="col-md-4 login">
