cc5bc43f235b08715d42ee37e82056268acf1430
Zack Glaser <zackglaser@gmail.com>
2018-05-17 10:38:20 -0500

    adding three buttons

--- index.php
+++ index.php
@@ -11,8 +11,14 @@ header_check();
   <div class="row">
     <div class="col-md-8">
       <h3 class="text-center">Create Document</h3>
-      <div class="col-md-12 docButton">
+      <div class="col-md-4 docButton">
         <h3>Residential Lease</h3>
+      </div>
+      <div class="col-md-4 docButton">
+        <h3>Simple Will</h3>
+      </div>
+      <div class="col-md-4 docButton">
+        <h3>Non-Discolure Agreement</h3>
       </div>
     </div>
     <div class="col-md-4 login">
