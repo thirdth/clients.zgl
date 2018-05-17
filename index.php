95301eb2dc5a2d523669bf8fe8d0463c2cb87d5a
Zack Glaser <zackglaser@gmail.com>
2018-05-17 09:57:55 -0500

    begin index.php

--- index.php
+++ index.php
@@ -1,8 +1,17 @@
 <?php
 include 'includes/garble_cnfg.php';
-header_check(); 
+header_check();
 ?>
-
+<div class="container">
+  <div class="row">
+    <div class="col-md-6">
+      <h3>Create Document</h3>
+    </div>
+    <div class="col-md-6">
+      <h3>Login/Signup</h3>
+    </div>
+  </div>
+<div>
 <?php
 
 include 'includes/footer.php'; ?>
