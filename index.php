b3a4210ecb502770370c7c637b17125be38d2062
Zack Glaser <zackglaser@gmail.com>
2018-05-17 12:48:53 -0500

    switch img

--- index.php
+++ index.php
@@ -5,13 +5,13 @@ header_check();
 <div class="container">
   <div class="row">
     <div class="col-md-12 welcome">
-      <div class="col-md-5">
-        <img src="/img/walker.jpg" width="450" height="250">
-      </div>
       <div class="col-md-7">
         <h2 class="text-center">Welcome<h2>
         <p>Welcome to Zack Glaser Legal self-service portal. If you don't already have an account, you can either create one now, or you can create one at checkout.
         </p>
+      </div>
+      <div class="col-md-5">
+        <img src="/img/walker.jpg" width="450" height="250">
       </div>
     </div>
   </div>
