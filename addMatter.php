bafe950a0efee3abd11d0a9d91e7fe5a7ccc3919
Zack Glaser <zackglaser@gmail.com>
2018-08-03 16:44:24 -0500

    added address and phone to client

--- addMatter.php
+++ addMatter.php
@@ -12,8 +12,11 @@ $client = get_client_byId($clientID);
   <div class="col-md-6">
     <?php
     echo "<h5><a href='/clientPage.php?ID=" . $client['ID'] . "'>" . $client['FName'] . " " . $client['LName'] . "</a></h5>";
-    echo "<h5>" . $client['Street1'] . "</h5>";
-    echo "<h5>" . $client['City'] . ", " . $client['State'] . " " . $client['Zip'] . "<h5>"; 
+    $address = get_address_byID($client['AddressID']);
+    echo "<h5>" . $address['Street1'] . "</h5>";
+    echo "<h5>" . $address['City'] . "<?h5>" . ", " . $address['State'] . " " . $address['Zip'] . "</h5>";
+    $phone = get_phone_byID($client['PhoneID']);
+    echo "<h5>" . $phone['Number'] . "</h5>";
     ?>
       <h2>New Matter</h2>
       <p>Please enter Matter information.</p>
