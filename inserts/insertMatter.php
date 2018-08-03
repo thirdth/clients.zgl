9bcb5e60abc5e930390a1988f8c198a800a4af97
Zack Glaser <zackglaser@gmail.com>
2018-08-03 16:53:36 -0500

    changed insertMatter

--- inserts/insertMatter.php
+++ inserts/insertMatter.php
@@ -3,25 +3,11 @@ error_reporting(-1);
 ini_set('display_errors', 'On');
 include '../includes/garble_cnfg.php';
 $Name = $_POST['Name'];
-$FName = $_POST['AdverseFirst'];
-$MName = $_POST['AdverseMiddle'];
-$LName = $_POST['AdverseLast'];
-$Street1 = $_POST['street1'];
-$Street2 = $_POST['street2'];
-$City = $_POST['city'];
-$State = $_POST['state'];
-$Zip = $_POST['zip'];
-$Phone = $_POST['phone'];
-$Email = $_POST['email'];
-$Notes = $_POST['Notes'];
-$clientID = $_POST['clientID'];
+$Description = $_POST['Description'];
+$MatterNumber = $_POST['MatterNum'];
 
-$PhoneID = insert_phone($Phone);
-$EmailID = insert_email($Email);
-$AddressID = insert_address($Street1, $Street2, $City, $State, $Zip);
-$AdverseID = insert_person($FName, $MName, $LName, $AddressID, $PhoneID, $EmailID);
 
-$result = insert_matter($Name, $clientID, $AdverseID, $Notes);
+$result = insert_matter($Name, $clientID, $Description, $MatterNum);
 
 if ($result)  {
   header("Location: ../clientPage.php?ID=" . $clientID);
