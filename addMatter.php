5dc412f8c8de89d704a3106f3eb2260b595131cf
Zack Glaser <zackglaser@gmail.com>
2018-08-03 16:33:23 -0500

    changin matter to less

--- addMatter.php
+++ addMatter.php
@@ -3,40 +3,36 @@ include 'includes/garble_cnfg.php';
 protected_page();
 header_check();
 $clientID = $_GET['ID'];
+$client = get_client_byId($clientID);
 ?>
 <div class="wrapper container">
-  <div class="col-md-1">
+
+  <div class="col-md-3">
   </div>
-  <div class="col-md-10">
+  <div class="col-md-6">
+    <?php
+    echo "<h5><a href='/clientPage.php?ID=" . $client['ID'] . "'>" . $client['FName'] . " " . $client['LName'] . "</a></h5>";
+    ?>
       <h2>New Matter</h2>
       <p>Please enter Matter information.</p>
-
       <form action="inserts/insertMatter.php" method="post">
-          <input type="hidden" name="clientID" value="<?php echo $clientID; ?>">
-        <div class="col-md-6">
+            <input type="hidden" name="clientID" value="<?php echo $clientID; ?>">
           <div class="form-group">
               <label>Name</label>
               <input type="text" name="Name" class="form-control">
               <span class="help-block"></span>
           </div>
-        </div>
-        <div class="col-md-6">
           <div class="form-group">
-            <label>Name</label>
-            <input type="text" name="Name" class="form-control">
-            <span class="help-block"></span>
+              <label>Description</label>
+              <textarea rows="10" name="Description" class="form-control"></textarea>
           </div>
-        </div>
-        <div class="col-md-6">
           <div class="form-group">
-            <label>Name</label>
-            <input type="text" name="Name" class="form-control">
-            <span class="help-block"></span>
+              <label>Matter Number</label>
+              <input type="text" name="MatterNum" class="form-control">
           </div>
-        </div>
-        <div class="form-group">
-            <input type="submit" class="btn btn-primary" name="submit" value="Login">
-        </div>
+          <div class="form-group">
+              <input type="submit" class="btn btn-primary" name="submit" value="Create">
+          </div>
       </form>
     </div>
   </div>
