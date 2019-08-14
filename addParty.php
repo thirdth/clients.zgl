8a5d3f1ae76ab2381791b08934bc22f9a3e228c5
Zack Glaser <zackglaser@gmail.com>
2019-08-14 18:16:23 -0500

    style form

--- addParty.php
+++ addParty.php
@@ -6,16 +6,18 @@ protected_page();
 header_check();
 $matterID = $_GET['ID'];
 $partyTypes = get_party_types();
-print_r($partyTypes);
+//print_r($partyTypes);
 ?>
 <div class="wrapper container">
   <div class="col-md-3">
   </div>
   <div class="col-md-6">
+    <h3>Add Party</h3>
+    <hr>
     <form action="inserts/insertParty.php" method="POST" class="form-group">
         <input type="hidden" name="MatterID" value="<? echo $matterID; ?>">
       <div class='col-md-12 form-group'>
-        <select name='PartyType'>
+        <select class='form-control' name='PartyType'>
           <?php
             foreach ($partyTypes as $party) {
               echo "<option value='" . $party['ID'] . "'>". $party['Name'] . "</option>";
@@ -25,37 +27,51 @@ print_r($partyTypes);
         </select>
       </div>
       <div class='col-md-4 form-group'>
-        <input type="text" name="FName">
+        <label>First/Company Name</label>
+        <input class="form-control" type="text" name="FName">
       </div>
       <div class='col-md-4 form-group'>
-        <input type="text" name="MName">
+        <label>Middle Name/Initial</label>
+        <input class="form-control" type="text" name="MName">
       </div>
       <div class='col-md-4 form-group'>
-        <input type="text" name="LName">
+        <label>Last Name</label>
+        <input class="form-control" type="text" name="LName">
       </div>
       <div class='col-md-12 form-group'>
-        <input type="text" name="Street1">
+        <label>Street</label>
+        <input class="form-control" type="text" name="Street1">
       </div>
       <div class='col-md-12 form-group'>
-        <input type="text" name="Street2">
+        <label>Street Line 2</label>
+        <input class="form-control" type="text" name="Street2">
       </div>
-      <div class='col-md-7'>
-        <input type="text" name="City">
+      <div class='col-md-7 form-group'>
+        <label>City</label>
+        <input class="form-control" type="text" name="City">
       </div>
-      <div class='col-md-2'>
-        <input type="text" name="State">
+      <div class='col-md-2 form-group'>
+        <label>State</label>
+        <input class="form-control" type="text" name="State">
       </div>
-      <div class='col-md-3'>
-        <input type="text" name="Zip">
+      <div class='col-md-3 form-group'>
+        <label>Zip</label>
+        <input class="form-control" type="text" name="Zip">
       </div>
-      <div class='col-md-5'>
-        <input type="text" name="Phone">
+      <div class='col-md-5 form-group'>
+        <label>Phone</label>
+        <input class="form-control" type="text" name="Phone">
       </div>
-      <div class='col-md-7'>
-        <input type="text" name="Email">
+      <div class='col-md-7 form-group'>
+        <label>Email Address</label>
+        <input class="form-control" type="email" name="Email">
       </div>
-      <div class='col-md-12'>
-        <button type="submit" class="btn btn-primary">Submit</button>
+      <div class="row">
+        <div class='col-md-12 form-group'>
+          <div class='col-md-4'>
+            <button type="submit" class="btn btn-primary">Submit</button>
+          </div>
+        </div>
       </div>
     </form>
   </div>
